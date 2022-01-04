64d32b0c878ec8345d856401ec123d546a7af8bf
EugeneGreen95 <don.t.st00000000p@gmail.com>
2022-01-04 22:23:30 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    n = String(n);
+    if(n.startsWith('-')) {
+        let newN = n.slice(1);
+        newN = newN.split('').reverse().join('');
+        return +newN;
+    } else {
+        n = n.split('').reverse().join('');
+        return +n;
+    }  
 }
